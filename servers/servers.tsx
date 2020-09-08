34a4113ac0b49f2f19baff020581592788a5dfc1
shentong <shentong@meishubao.com>
2020-09-08 21:01:43 +0800

    request

---
+++ servers/servers.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+import HTTPREQUEST from "./http";
+
+/* 获取IP信息 */
+// export function getIpInfo() {
+//   return HTTP.get('https://ipinfo.tianbaows.com');
+// }
+
+export function getResultData(postData) {
+  return HTTPREQUEST.post("/api/white-screen/search", postData);
+}
